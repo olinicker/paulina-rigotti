2b722f8b7e8c46dd3d2bb3dbcff1020e5b88ad1a
Walter <wdiasmarquespereira@gmail.com>
2025-02-13 16:11:47 -0300

    Alterações de ordenação

--- index.js
+++ index.js
@@ -4,37 +4,42 @@ const repositoryName = "escola-teste";
 const client = prismic.createClient(repositoryName);
 
 const init = async () => {
-  const response = await client.getAllByType("atividade-projeto", {
-    orderings: "my.post.date desc",
-  });
+  try {
+    const response = await client.getAllByType("atividade-projeto");
 
-  const posts = response.map(({ uid, data: post }) => {
-    return {
+    const posts = response.sort((a, b) => b.uid.localeCompare(a.uid));
+
+    const formattedPosts = posts.map(({ uid, data: post }) => ({
       id: uid,
       title: prismic.asText(post.title),
       description: prismic.asText(post.description),
       thumbnail: post.thumbnail.url,
-    };
-  });
+    }));
 
-  posts.forEach((post) => {
-    const postAsHtml = `
-      <article class="project-item">
-        <div class="project-content">
-          <h3>${post.title}</h3>
-          <p>${post.description}</p>
-        </div>
-        <div class="project-image">
-          <img
-            src="${post.thumbnail}"
-            alt="${post.title}"
-          />
-        </div>
-      </article>
-    `;
-  
-    document.querySelector(".project-list").innerHTML += postAsHtml;
-  });  
+    let postsHtml = "";
+    formattedPosts.forEach((post) => {
+      postsHtml += `
+        <article class="project-item">
+          <div class="project-content">
+            <h3>${post.title}</h3>
+            <p>${post.description}</p>
+          </div>
+          <div class="project-image">
+            <img src="${post.thumbnail}" alt="${post.title}" />
+          </div>
+        </article>
+      `;
+    });
+
+    const projectList = document.querySelector(".project-list");
+    if (projectList) {
+      projectList.innerHTML = postsHtml;
+    }
+  } catch (error) {
+    console.error("Erro ao buscar posts:", error);
+    document.querySelector(".project-list").innerHTML =
+      "<p>Erro ao carregar os projetos.</p>";
+  }
 };
 
 init();
